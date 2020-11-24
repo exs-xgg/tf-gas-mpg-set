8eea3d15f5b485f445ee2adc1cc69ae8a6e9f53d
Romeo David <romeo@fullstack.ph>
2020-11-25 00:44:02 +0800

    for testing to nitro

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-
+import sys
 
 # Make numpy printouts easier to read.
 np.set_printoptions(precision=3, suppress=True)
@@ -12,21 +12,6 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.layers.experimental import preprocessing
-
-def plot_loss(history):
-  plt.plot(history.history['loss'], label='loss')
-  plt.plot(history.history['val_loss'], label='val_loss')
-  plt.ylim([0, 10])
-  plt.xlabel('Epoch')
-  plt.ylabel('Error [MPG]')
-  plt.legend()
-  plt.grid(True)
-def plot_horsepower(x, y):
-  plt.scatter(train_features['Horsepower'], train_labels, label='Data')
-  plt.plot(x, y, color='k', label='Predictions')
-  plt.xlabel('Horsepower')
-  plt.ylabel('MPG')
-  plt.legend()
 
 print(tf.__version__)
 
@@ -38,19 +23,20 @@ raw_dataset = pd.read_csv(url, names=column_names,
                           na_values='?', comment='\t',
                           sep=' ', skipinitialspace=True)
 
+
 dataset = raw_dataset.copy()
-dataset.tail()
+
 dataset.isna().sum()
 dataset = dataset.dropna()
+
+
 dataset['Origin'] = dataset['Origin'].map({1: 'USA', 2: 'Europe', 3: 'Japan'})
 
 dataset = pd.get_dummies(dataset, prefix='', prefix_sep='')
-dataset.tail()
 
 train_dataset = dataset.sample(frac=0.8, random_state=0)
 test_dataset = dataset.drop(train_dataset.index)
-sns.pairplot(train_dataset[['MPG', 'Cylinders', 'Displacement', 'Weight']], diag_kind='kde')
-
+#sns.pairplot(train_dataset[['MPG', 'Cylinders', 'Displacement', 'Weight']], diag_kind='kde')
 
 train_dataset.describe().transpose()
 
@@ -64,8 +50,10 @@ train_dataset.describe().transpose()[['mean', 'std']]
 
 
 normalizer = preprocessing.Normalization()
+
 normalizer.adapt(np.array(train_features))
-print(normalizer.mean.numpy())
+
+normalizer.mean.numpy()
 
 
 first = np.array(train_features[:1])
@@ -75,17 +63,31 @@ with np.printoptions(precision=2, suppress=True):
   print()
   print('Normalized:', normalizer(first).numpy())
 
+
 horsepower = np.array(train_features['Horsepower'])
-horsepower_normalizer = preprocessing.Normalization(input_shape=[1,])
+
+
+horsepower_normalizer = preprocessing.Normalization(input_shape=[1])
 horsepower_normalizer.adapt(horsepower)
+
+
+
 horsepower_model = tf.keras.Sequential([
     horsepower_normalizer,
     layers.Dense(units=1)
 ])
 
+
 horsepower_model.summary()
 
-horsepower_model.predict(horsepower[:10])
+
+print('------')
+print(horsepower_model.predict(horsepower[:10]))
+sys.exit()
+#print(horsepower_model.predict(horsepower[:10]))
+
+
+
 horsepower_model.compile(
     optimizer=tf.optimizers.Adam(learning_rate=0.1),
     loss='mean_absolute_error')
@@ -102,8 +104,20 @@ history = horsepower_model.fit(
 hist = pd.DataFrame(history.history)
 hist['epoch'] = history.epoch
 
-plot_loss(history)
 
+
+def plot_loss(history):
+  plt.plot(history.history['loss'], label='loss')
+  plt.plot(history.history['val_loss'], label='val_loss')
+  plt.ylim([0, 10])
+  plt.xlabel('Epoch')
+  plt.ylabel('Error [MPG]')
+  plt.legend()
+  plt.grid(True)
+  
+plot_loss(history)
+  
+sys.exit()
 test_results = {}
 
 test_results['horsepower_model'] = horsepower_model.evaluate(
@@ -115,6 +129,13 @@ x = tf.linspace(0.0, 250, 251)
 y = horsepower_model.predict(x)
 
 
-plot_horsepower(x,y)
+def plot_horsepower(x, y):
+  plt.scatter(train_features['Horsepower'], train_labels, label='Data')
+  plt.plot(x, y, color='k', label='Predictions')
+  plt.xlabel('Horsepower')
+  plt.ylabel('MPG')
+  plt.legend()
+
+#plot_horsepower(x,y)
 
 
